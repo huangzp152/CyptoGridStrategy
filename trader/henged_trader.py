21bedf7b8a2c20673f0d0e65466d194f881196cc
huangziping <huangziping@qiyi.com>
2021-07-30 09:56:08 +0800

    修复接下来的买卖价格不准确的问题

--- trader/henged_trader.py
+++ trader/henged_trader.py
@@ -305,6 +305,7 @@ class HengedGrid(object):
                     self.set_spot_next_sell_price(float(self.cur_market_future_price))
                     self.set_future_next_buy_price(float(self.cur_market_future_price))
                     self.set_future_next_sell_price(float(self.cur_market_future_price))
+                    self.adjust_prices()
                     # self.set_spot_price(float(self.cur_market_future_price))
                     # self.set_future_price(float(self.cur_market_future_price))
 
@@ -614,28 +615,45 @@ class HengedGrid(object):
         demical_point = len(price_str_list[1]) if len(price_str_list) > 1 else 0 + 2
         dynamicConfig.spot_buy_price = round(min(float(dynamicConfig.record_future_price[-1]) if len(dynamicConfig.record_future_price) > 0 else 999999, deal_price * (1 - dynamicConfig.spot_falling_ratio / 100)), demical_point)  # 多单跌的时候补仓 # 保留2位小数
         self.spot_buy_price = dynamicConfig.spot_buy_price
-        print("设置接下来多单买入的价格, " + str(self.spot_buy_price))
+        # print("设置接下来多单买入的价格, " + str(self.spot_buy_price))
 
     def set_spot_next_sell_price(self, deal_price):
         price_str_list = str(deal_price).split(".")
         demical_point = len(price_str_list[1]) if len(price_str_list) > 1 else 0 + 2
-        dynamicConfig.spot_sell_price = round(max(float(dynamicConfig.record_future_price[-1]) if len(dynamicConfig.record_future_price) > 0 else 0, deal_price * (1 + dynamicConfig.spot_rising_ratio / 100)), demical_point)
+        dynamicConfig.spot_sell_price = round(max(float(dynamicConfig.record_spot_price[-2]) if len(dynamicConfig.record_spot_price) > 1 else 0, float(dynamicConfig.record_future_price[-1]) if len(dynamicConfig.record_future_price) > 0 else 0, deal_price * (1 + dynamicConfig.spot_rising_ratio / 100)), demical_point)
         self.spot_sell_price = dynamicConfig.spot_sell_price
-        print("设置接下来多单卖出的价格:" + str(self.spot_sell_price))
+        # print("设置接下来多单卖出的价格:" + str(self.spot_sell_price))
 
     def set_future_next_sell_price(self, deal_price):
         price_str_list = str(deal_price).split(".")
         demical_point = len(price_str_list[1]) if len(price_str_list) > 1 else 0 + 2
         dynamicConfig.future_sell_price = round(max(float(dynamicConfig.record_spot_price[-2]) if len(dynamicConfig.record_spot_price) > 1 else 0, deal_price * (1 + dynamicConfig.future_falling_ratio / 100)), demical_point)
         self.future_sell_price = dynamicConfig.future_sell_price
-        print("设置接下来新开空单卖出的价格, " + str(self.future_sell_price))
+        # print("设置接下来新开空单卖出的价格, " + str(self.future_sell_price))
 
     def set_future_next_buy_price(self, deal_price):
         price_str_list = str(deal_price).split(".")
         demical_point = len(price_str_list[1]) if len(price_str_list) > 1 else 0 + 2
-        dynamicConfig.future_buy_price = round(min(float(dynamicConfig.record_spot_price[-2]) if len(dynamicConfig.record_spot_price) > 1 else 999999, deal_price * (1 - dynamicConfig.future_rising_ratio / 100)), demical_point)  #空单涨的时候补仓 # 保留2位小数
+        dynamicConfig.future_buy_price = round(min(float(dynamicConfig.record_future_price[-2]) if len(dynamicConfig.record_future_price) > 1 else 999999, float(dynamicConfig.record_spot_price[-2]) if len(dynamicConfig.record_spot_price) > 1 else 999999, deal_price * (1 - dynamicConfig.future_rising_ratio / 100)), demical_point)  #空单涨的时候补仓 # 保留2位小数
         self.future_buy_price = dynamicConfig.future_buy_price
-        print("设置接下来空单的买回价格:" + str(self.future_buy_price))
+        # print("设置接下来空单的买回价格:" + str(self.future_buy_price))
+
+    def adjust_prices(self):
+        min_buy_price = min(dynamicConfig.spot_buy_price, dynamicConfig.future_buy_price)
+        max_sell_price = max(dynamicConfig.spot_sell_price, dynamicConfig.future_sell_price)
+        dynamicConfig.spot_buy_price = min_buy_price
+        dynamicConfig.spot_sell_price = max_sell_price
+        dynamicConfig.future_sell_price = max_sell_price
+        dynamicConfig.future_buy_price = min_buy_price
+        self.spot_buy_price = dynamicConfig.spot_buy_price
+        self.spot_sell_price = dynamicConfig.spot_sell_price
+        self.future_sell_price = dynamicConfig.future_sell_price
+        self.future_buy_price = dynamicConfig.future_buy_price
+        print("【重设】接下来多单买入的价格, " + str(self.spot_buy_price))
+        print("【重设】接下来多单卖出的价格:" + str(self.spot_sell_price))
+        print("【重设】接下来新开空单卖出的价格, " + str(self.future_sell_price))
+        print("【重设】接下来空单的买回价格:" + str(self.future_buy_price))
+        pass
 
     def set_spot_share(self, spot_step):
         print("set_spot_share")
